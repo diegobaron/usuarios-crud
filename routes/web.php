cdf29aa0513dd938e9827ba6b104d232a0a2265d
Diego Baron <diegobaron_bjo@hotmail.com>
2021-11-09 15:12:53 -0300

    ajuste web routes

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,11 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('users', UserController::class);
 
 Route::fallback(function () {
-    abort(404);
+    return redirect()->route('home');
 });
